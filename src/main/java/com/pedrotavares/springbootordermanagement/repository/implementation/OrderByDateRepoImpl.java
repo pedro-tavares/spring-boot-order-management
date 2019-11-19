3c721023f71f8706708c5616db28fb9bd9837b42
Pedro Alexandre Tavares <patavares@outlook.com>
2019-11-19 19:18:16 +0000

    Refactor Repo Implementation

---
+++ src/main/java/com/pedrotavares/springbootordermanagement/repository/implementation/OrderByDateRepoImpl.java
@@ -0,0 +1,26 @@
+package com.pedrotavares.springbootordermanagement.repository.implementation;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.pedrotavares.springbootordermanagement.model.TheOrder;
+import com.pedrotavares.springbootordermanagement.repository.OrderByDateRepo;
+
+@Component
+public class OrderByDateRepoImpl implements OrderByDateRepo {
+	
+	@Autowired
+	private EntityManager em;
+
+	public List<TheOrder> findByDate(Date startDate, Date endDate) {
+		return em.createQuery("SELECT o FROM TheOrder o WHERE o.orderDate>:startDate AND o.orderDate<:endDate")
+			.setParameter("startDate", startDate)
+			.setParameter("endDate", endDate)
+			.setMaxResults(10).getResultList();
+	}
+}
